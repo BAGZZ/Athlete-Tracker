90c324603eab58a3ea27effa5c0d108e1c978160
mikeArt <arterburnmj@grizzlies.adams.edu>
2015-05-26 11:46:03 -0600

    Create AthleteTrackerDatabase.java

---
+++ src/edu/adams/backendboys/AthleteTrackerDatabase.java
@@ -0,0 +1,11 @@
+package edu.adams.backendboys;
+
+import java.util.ArrayList;
+
+public class AthleteTrackerDatabase {
+	private Database database;
+	
+	public ArrayList<Athlete> searchDatabase(){
+		return null;
+	}
+}
