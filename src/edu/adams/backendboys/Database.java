3ef6d5d5313e2404418cce567e55c1a6734966bb
mikeArt <arterburnmj@grizzlies.adams.edu>
2015-05-27 09:33:27 -0600

    Update Database.java
    This was really Jeremiah.

--- src/edu/adams/backendboys/Database.java
+++ src/edu/adams/backendboys/Database.java
@@ -2,4 +2,10 @@ package edu.adams.backendboys;
 
 public abstract class Database {
 
+	public abstract Boolean insert(String table,String[] data);
+	
+	public abstract String[] select(String table,String[] data);
+	
+	public abstract Boolean update(String table,String[] data);
+	
 }
